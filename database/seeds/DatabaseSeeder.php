d0fcf1260c76d7c312d183dd172dc6b524a059db
Regynald Zambrano <zreyleo@gmail.com>
2021-11-13 18:17:28 -0500

    clase-05: sanctum

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(CategorySeeder::class);
         $this->call(ProductSeeder::class);
     }
 }
